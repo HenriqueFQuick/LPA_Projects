1cc52470b107bfa6a98c13446507d5e529162391
Henrique Fernandes <henrique.fquick@gmail.com>
2020-02-22 17:46:49 -0300

    Finalizado justificador.cpp

--- justificador.cpp
+++ justificador.cpp
@@ -1,59 +1,40 @@
 #include <iostream>
 #include <string.h>
-#include <ctype.h>
 
-#include <typeinfo>
 using namespace std;
 
-
-void metodo(string str){
-    int inicio = 0; 
-    int final = 0;
-
-    for(int i = 0; i < str.length(); i++){
-        if(!isspace(str[i])){
-            final = i;
-            str.erase(inicio, final);
-        }
-    }
-    cout << str;
-}
 
 int main(){
 
     int quantidade = 0;
     cin >> quantidade;
+    cin.ignore();
 
     while(quantidade != 0){
 
         string* frases = new string[quantidade];
+        string frase = "";
+        string fraseAux = "";
 
         for(int k = 0; k < quantidade; k++){
 
-            string frase = "";
-            string fraseAux = "";
+            frase = "";
+            fraseAux = "";
 
-            cin.ignore();
-            getline( cin, frase);
+            getline(cin, frase);
 
-
-            /*int inicio = 0; 
-            int final = 0;
-            bool teste = true;
-
-            for(int p = 0; p < frase.length() && teste; p++){
-                if(!isspace(frase[p])){
-                    final = p;
-                    frase  = frase.erase(inicio, final);
-                    cout << frase << "\n";
-                    teste = false;
-                }
-            }
-            cout << frase;    TESTE  */
+            //cout << frase << "\n";
+            
+            bool isbegining = true;
 
             for(int i = 0; i < frase.length(); i++){
-                //cout << "FRASE: " << frase[i] << " ISPACE: " << isspace(frase[i]) << "\n";
-                if(!(isspace(frase[i]) && (i+1 < frase.length()) && isspace(frase[i+1]))){  
+                //cout << "FRASE: " << frase << " i: " << i << " frase[i]: " << frase[i] <<"\n"; 
+                if(isbegining){
+                    if(!isspace(frase[i])){  
+                        fraseAux=frase[i];
+                        isbegining = false;
+                    }
+                }else if(!(isspace(frase[i]) && (i+1 < frase.length()) && isspace(frase[i+1]))){  
                     fraseAux+=frase[i];  
                 }
                 
@@ -64,12 +45,9 @@ int main(){
         int max = frases[0].length();
         for(int k = 0; k < quantidade; k++){
             if(frases[k].length() > max){
-                max = frases[k].length();    
-                //cout << frases[k];           
-            } 
-            //cout << frases[k] << "\n";
+                max = frases[k].length();  
+            }
         }
-        //cout << max << "\n";
 
         string* resultado = new string[quantidade];
 
@@ -84,8 +62,7 @@ int main(){
             cout << resultado[k] << "\n";
         }
 
-
         cin >> quantidade;
-
+        cin.ignore();
     }
 }
